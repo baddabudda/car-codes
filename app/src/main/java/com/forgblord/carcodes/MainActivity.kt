214106a5c76d91ec80e13840f1774a2459f64ff9
baddabudda <radkova.irene@gmail.com>
2025-03-06 20:11:35 +0300

    feat: add SearchView to top app bar menu item

--- app/src/main/java/com/forgblord/carcodes/MainActivity.kt
+++ app/src/main/java/com/forgblord/carcodes/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.forgblord.carcodes
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var topAppbar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,10 +25,33 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        buildSubjectsRecyclerView()
+        topAppbarHandler()
+    }
+
+    private fun buildSubjectsRecyclerView() {
         val subjectsRecyclerView: RecyclerView = findViewById(R.id.rv_subjects)
         val subjectsListAdapter = FederalSubjectsListAdapter()
 
         subjectsRecyclerView.layoutManager = LinearLayoutManager(this)
         subjectsRecyclerView.adapter = subjectsListAdapter
+    }
+
+    private fun topAppbarHandler() {
+        topAppbar = findViewById(R.id.top_app_bar)
+
+        topAppbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    // TODO: add searching function
+                    true
+                }
+                R.id.sort -> {
+                    // TODO: add sorting function
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
